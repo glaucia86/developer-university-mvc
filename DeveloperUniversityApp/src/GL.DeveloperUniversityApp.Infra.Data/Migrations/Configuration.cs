e38420243eb6ea3e75f12c8825b20f502d2086cb
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-08-17 19:58:13 -0300

    Execução do EF via Package Manager.

---
+++ DeveloperUniversityApp/src/GL.DeveloperUniversityApp.Infra.Data/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+namespace GL.DeveloperUniversityApp.Infra.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Context.SchoolContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Context.SchoolContext context)
+        {
+            
+        }
+    }
+}
