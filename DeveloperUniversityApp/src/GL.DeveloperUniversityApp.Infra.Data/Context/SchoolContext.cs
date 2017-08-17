5ea62db44bf277718ce5af1832727a1bced367fd
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-08-17 19:43:42 -0300

    Inclusão de connectionString no WebConfig

--- DeveloperUniversityApp/src/GL.DeveloperUniversityApp.Infra.Data/Context/SchoolContext.cs
+++ DeveloperUniversityApp/src/GL.DeveloperUniversityApp.Infra.Data/Context/SchoolContext.cs
@@ -1,9 +1,21 @@
 ﻿
+using System.Data.Entity;
+using GL.DeveloperUniversityApp.Domain.Entities;
 
 namespace GL.DeveloperUniversityApp.Infra.Data.Context
 {
-    public class SchoolContext
+    public class SchoolContext : DbContext        
     {
+        public SchoolContext() : base("DeveloperUniversity")
+        {
+            //Default
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
 
     }
 }
