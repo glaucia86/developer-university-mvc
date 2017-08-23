7b05653e83d9d03b28cee6b3265ef90eb830b965
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-08-23 20:25:01 -0300

    Pequenas alterações no arquivo 'Configuration.cs'

--- DeveloperUniversityApp/src/GL.DeveloperUniversityApp.Infra.Data/Migrations/Configuration.cs
+++ DeveloperUniversityApp/src/GL.DeveloperUniversityApp.Infra.Data/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
 namespace GL.DeveloperUniversityApp.Infra.Data.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+
 
     internal sealed class Configuration : DbMigrationsConfiguration<Context.SchoolContext>
     {
@@ -14,7 +16,7 @@ namespace GL.DeveloperUniversityApp.Infra.Data.Migrations
 
         protected override void Seed(Context.SchoolContext context)
         {
-            
+
         }
     }
 }
