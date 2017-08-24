e6913893e2b9eec4172d4a38ead2397d908765d9
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-08-24 20:09:52 -0300

    Desenvolvimento do arquivo 'StudentConfiguration' infos inerentes a classe Student

---
+++ DeveloperUniversityApp/src/GL.DeveloperUniversityApp.Infra.Data/EntityConfig/StudentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using GL.DeveloperUniversityApp.Domain.Entities;
+
+namespace GL.DeveloperUniversityApp.Infra.Data.EntityConfig
+{
+    /// <summary>
+    /// Classe responsável por adotar Configurações especiais inerentes a classe: Student
+    /// </summary>
+    public class StudentConfiguration : EntityTypeConfiguration<Student>
+    {
+        public StudentConfiguration()
+        {
+            HasKey(s => s.StudentId);
+
+            Property(s => s.FirstMidName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            Property(s => s.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
