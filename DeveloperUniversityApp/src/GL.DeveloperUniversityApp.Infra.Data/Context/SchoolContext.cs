94ed7470191ddc830674b47ac791bd13729ee0a6
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-08-23 21:34:02 -0300

    Desenvolvimento de novas convenções para o projeto.

--- DeveloperUniversityApp/src/GL.DeveloperUniversityApp.Infra.Data/Context/SchoolContext.cs
+++ DeveloperUniversityApp/src/GL.DeveloperUniversityApp.Infra.Data/Context/SchoolContext.cs
@@ -25,6 +25,19 @@ namespace GL.DeveloperUniversityApp.Infra.Data.Context
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
             modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
             modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            //aqui estou identificando quem é a PK da Table
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            //Configuração dos tipos dos campos que sejam 'varchar'
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            //Configuração dos tamanhos dos campos do tipo 'varchar' (padrão: 100)
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
         }
     }
 }
