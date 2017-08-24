7724bcd33c796303b1f2ca3e40106c5a084a78d8
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-08-23 21:25:51 -0300

    Criação de um método para realização de convenções do EF.

--- DeveloperUniversityApp/src/GL.DeveloperUniversityApp.Infra.Data/Context/SchoolContext.cs
+++ DeveloperUniversityApp/src/GL.DeveloperUniversityApp.Infra.Data/Context/SchoolContext.cs
@@ -1,12 +1,14 @@
 ﻿
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using GL.DeveloperUniversityApp.Domain.Entities;
 
 namespace GL.DeveloperUniversityApp.Infra.Data.Context
 {
     public class SchoolContext : DbContext        
     {
-        public SchoolContext() : base("DeveloperUniversity")
+        public SchoolContext() 
+            : base("DeveloperUniversity")
         {
             //Default
         }
@@ -17,5 +19,12 @@ namespace GL.DeveloperUniversityApp.Infra.Data.Context
 
         public DbSet<Course> Courses { get; set; }
 
+        //Método responsável por realizar convenções das tables criadas no EF
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
     }
 }
